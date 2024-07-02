532d697bc91e779c4560a19dfb0ee0a9277c4fa3
edustorg <edustorg@gmail.com>
2024-07-02 22:52:09 +0600

    basic setup using vite react ts-swc shadcn-ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@/components/ui/typography";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center flex-col gap-4">
+        <Typography variant="h1">Welcome to our Edust!</Typography>
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
