cd6e4c82be11b3e532bda80a2ded224cdbd35fec
edustorg <edustorg@gmail.com>
2024-07-08 17:10:56 +0600

    gsPluginNewsLetter installed

---
+++ src/organizations/features/sites-builder/grapesjs/index.tsx
@@ -0,0 +1,30 @@
+import grapesjs, { Editor } from "grapesjs";
+import GjsEditor from "@grapesjs/react";
+
+import gsPluginBlocksBasic from "grapesjs-blocks-basic";
+import gsPluginNewsLetter from "grapesjs-preset-newsletter";
+
+export const GrapesJs = () => {
+  const onEditor = (editor: Editor) => {
+    console.log("Editor loaded", { editor });
+  };
+  return (
+    <>
+      <GjsEditor
+        // Pass the core GrapesJS library to the wrapper (required).
+        // You can also pass the CDN url (eg. "https://unpkg.com/grapesjs")
+        grapesjs={grapesjs}
+        // Load the GrapesJS CSS file asynchronously from URL.
+        // This is an optional prop, you can always import the CSS directly in your JS if you wish.
+        grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
+        // GrapesJS init options
+        options={{
+          height: "100vh",
+          storageManager: false,
+        }}
+        onEditor={onEditor}
+        plugins={[gsPluginBlocksBasic, gsPluginNewsLetter]}
+      />
+    </>
+  );
+};
--- src/organizations/features/sites-builder/index.tsx
+++ src/organizations/features/sites-builder/index.tsx
@@ -1,32 +1,9 @@
-import grapesjs, { Editor } from "grapesjs";
-import GjsEditor from "@grapesjs/react";
-
-import gsBlocksBasic from "grapesjs-blocks-basic";
-
-// style
-import "./grapesjs/style.css";
+import { GrapesJs } from "./grapesjs";
 
 export const SitesBuilder = () => {
-  const onEditor = (editor: Editor) => {
-    console.log("Editor loaded", { editor });
-  };
   return (
-    <>
-      <GjsEditor
-        // Pass the core GrapesJS library to the wrapper (required).
-        // You can also pass the CDN url (eg. "https://unpkg.com/grapesjs")
-        grapesjs={grapesjs}
-        // Load the GrapesJS CSS file asynchronously from URL.
-        // This is an optional prop, you can always import the CSS directly in your JS if you wish.
-        grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
-        // GrapesJS init options
-        options={{
-          height: "100vh",
-          storageManager: false,
-        }}
-        onEditor={onEditor}
-        plugins={[gsBlocksBasic]}
-      />
-    </>
+    <div>
+      <GrapesJs />
+    </div>
   );
 };
