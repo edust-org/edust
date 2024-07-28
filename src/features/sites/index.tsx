d565537fc41e8f18fa0d0567eefe4817856c811d
edustorg <edustorg@gmail.com>
2024-07-28 17:39:11 +0600

    show data in sites

--- src/features/sites/index.tsx
+++ src/features/sites/index.tsx
@@ -1,13 +1,34 @@
-import { Button, Typography } from "@/components/ui";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export const Sites = () => {
+  const params = useParams();
+  console.log(params);
+  const [htmlData, setHtmlData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/organization_page/f634")
+      .then((data) => setHtmlData(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (htmlData?.css) {
+      const style = document.createElement("style");
+      style.type = "text/css";
+      style.innerHTML = htmlData.css;
+
+      document.head.appendChild(style);
+    }
+  }, [htmlData?.css]);
+
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="text-center space-y-4">
-        <Typography variant="h1">Your Organization Name</Typography>
-        <Typography variant="h2">organization-user-name</Typography>
-        <Button>Customize your sites</Button>
-      </div>
-    </div>
+    <>
+      {htmlData && (
+        <div dangerouslySetInnerHTML={{ __html: htmlData?.html }}></div>
+      )}
+    </>
   );
 };
