df847539da5b68833c061d55b08286b603458782
edustorg <edustorg@gmail.com>
2024-08-02 17:18:15 +0600

    add axios credentials

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes";
+import axios from "axios";
+
+// Axios - Set default configurations
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
