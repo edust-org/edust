f0bf301a6660910711eaf6203d7f8ae5c48936a0
edustorg <edustorg@gmail.com>
2024-08-07 11:53:07 +0600

    verification

--- src/features/authentication/verify-email-by-token.tsx
+++ src/features/authentication/verify-email-by-token.tsx
@@ -1,7 +1,9 @@
 import { useVerifyEmailByTokenMutation } from "@/app/api/v0/auth";
+import assets from "@/assets/images";
+import { Typography } from "@/components/ui";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { BeatLoader } from "react-spinners";
 export const VerifyEmailByToken: React.FC = () => {
   const params = useParams();
 
@@ -17,5 +19,18 @@ export const VerifyEmailByToken: React.FC = () => {
     }
   }, [params.token, verify]);
 
-  return <div>VerifyEmailByToken</div>;
+  return (
+    <div className="h-screen flex items-center justify-center p-4">
+      <div className="shadow p-4 md:p-6 w-full sm:max-w-96 md:max-w-[450px]">
+        <div className="text-center space-y-4">
+          <img src={assets.logo} alt="" className="mx-auto" width={250} />
+          <div className="space-y-2">
+            <Typography variant="h3">Verifying Your Account</Typography>
+            {isLoading && <BeatLoader />}
+          </div>
+          <Typography>Working for verification</Typography>
+        </div>
+      </div>
+    </div>
+  );
 };
