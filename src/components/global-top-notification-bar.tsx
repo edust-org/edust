111e39e8af87aae40369d632ab090ce4e0dae43c
edustorg <edustorg@gmail.com>
2024-10-07 23:21:20 +0600

    component(notification-bar): global top notification bar added

---
+++ src/components/global-top-notification-bar.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "./ui";
+
+export const GlobalTopNotificationBar = () => {
+  return (
+    <div className="bg-gradient-to-t from-gray-800 to-gray-950">
+      <Typography className="px-2 py-1 text-center text-xs text-white">
+        Edust - Development in Progress
+        <a href="https://github.com/edust-org/edust">(v0.0.0)</a>
+      </Typography>
+    </div>
+  );
+};
