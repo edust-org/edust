c4a951eb710427e5332189528e5cf2df6f949478
edustorg <edustorg@gmail.com>
2024-10-08 09:35:22 +0600

    backup delete and fix logo in loading

--- src/components/loading/spinner.tsx
+++ src/components/loading/spinner.tsx
@@ -1,14 +1,14 @@
-import assets from "@/assets/images";
+import { LogoEdust } from "../logo-edust";
 
 export const Spinner = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="flex h-screen items-center justify-center">
       <div className="space-y-4">
-        <img src={assets.logo} alt="Logo" />
+        <LogoEdust />
         <div role="status" className="flex items-center justify-center">
           <svg
             aria-hidden="true"
-            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-primary"
+            className="h-8 w-8 animate-spin fill-primary text-gray-200 dark:text-gray-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
