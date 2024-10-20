9e7037d5b2a0e0a662d3a2361dcfb35fdea852da
edustorg <edustorg@gmail.com>
2024-10-20 16:53:15 +0600

    fix(packages): rename pkg names

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
+import { Button } from "@edust/ui/button";
 import styles from "./page.module.css";
 
 export default function Home() {
