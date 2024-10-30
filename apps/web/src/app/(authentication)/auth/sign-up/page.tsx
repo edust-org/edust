cdc7013ce0e77c0f5460d58eef51f30e0b05d214
edustorg <edustorg@gmail.com>
2024-10-30 15:22:20 +0600

    feat(auth, institutes): empty with route setup

---
+++ apps/web/src/app/(authentication)/auth/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@/components/ui";
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="container">
+      <Typography variant="h1">Sign Up Page</Typography>
+    </main>
+  );
+}
