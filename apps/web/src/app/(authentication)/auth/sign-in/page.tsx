d5d299df34b9140f653247387d24147067aa438d
edustorg <edustorg@gmail.com>
2024-10-30 21:28:37 +0600

    rename(page): component rename page

--- apps/web/src/app/(authentication)/auth/sign-in/page.tsx
+++ apps/web/src/app/(authentication)/auth/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@/components/ui";
 
-export default function Page(): JSX.Element {
+export default function SignIn(): JSX.Element {
   return (
     <main className="container">
       <Typography variant="h1">Sign In Page</Typography>
