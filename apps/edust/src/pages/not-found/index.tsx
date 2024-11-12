e9790e5927d1a79698c4d01510b9be80af2985cc
edustorg <edustorg@gmail.com>
2024-11-12 22:23:38 +0600

    fix(404): page not found

--- apps/edust/src/pages/not-found/index.tsx
+++ apps/edust/src/pages/not-found/index.tsx
@@ -1,9 +1,27 @@
-import { useNavigate } from "react-router-dom";
-import { Button } from "@/components/ui";
-import { LogoEdust } from "@/components";
+import { useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui"
+import { LogoEdust } from "@/components"
+import { useAppSelector } from "@/app/hooks"
+import { Roles } from "@/types"
+import { useEffect } from "react"
 
 export const NotFound = () => {
-  const navigate = useNavigate();
+  const activeMode = useAppSelector(
+    (state) => state.auth.profileSwitch.activeMode,
+  )
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (
+      activeMode === Roles.USER ||
+      activeMode === Roles.OWNER ||
+      activeMode === Roles.EDITOR
+    ) {
+      return navigate("/")
+    }
+  }, [activeMode])
+
   return (
     <div className="h-svh">
       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
@@ -22,5 +40,5 @@ export const NotFound = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
