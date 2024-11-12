b2b327bbf76b86d1e0f92c9d5781bfbc514be268
GmMamunH <gmmamun3004@gmail.com>
2024-11-12 15:41:56 +0600

    update(contact-page) form, newsletter, and contact info
    [Done] contact text component done
    
    [Add] contact newsletter component add
    
    [Fixed] contact form input filed fixed

---
+++ apps/edust/src/pages/contact-us/contact-newsletter.tsx
@@ -0,0 +1,26 @@
+import { Button, Input } from "@/components/ui";
+
+export const ContactNewsletter = () => {
+  return (
+    <div className="container mt-10 flex flex-col gap-5 rounded bg-slate-50 p-6 md:flex-row md:items-center md:justify-between">
+      <div>
+        <p className="text-2xl font-semibold text-black">
+          Join Our Weekly Newsletter
+        </p>
+        <p className="text-base text-slate-900">
+          Stay up to date with the latest news, announcements, and insightful
+          articles.
+        </p>
+      </div>
+      <div>
+        {" "}
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="email" placeholder="Email" />
+          <Button size="sm" type="submit">
+            Subscribe
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
