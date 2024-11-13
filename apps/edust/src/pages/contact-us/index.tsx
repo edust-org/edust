a0b08c58b200b77e0d91488f574e7259ed8451b9
GmMamunH <gmmamun3004@gmail.com>
2024-11-13 18:41:40 +0600

    fix(contact-page): add navbar

--- apps/edust/src/pages/contact-us/index.tsx
+++ apps/edust/src/pages/contact-us/index.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components";
 import { ContactForm } from "./contact-form";
 import { ContactNewsletter } from "./contact-newsletter";
 import { ContactText } from "./contact-text";
@@ -5,7 +6,8 @@ import { ContactText } from "./contact-text";
 export const ContactUs = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row gap-4 md:gap-20 justify-center">
+    <Navbar.Guest/>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-20 justify-center pt-16">
         <ContactForm />
         <ContactText/>
       </div>
