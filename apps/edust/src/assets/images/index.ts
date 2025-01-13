cdfc20a594c98b7beb0e1b5749eb375c73eb37d8
GmMamunH <gmmamun3004@gmail.com>
2025-01-13 19:02:30 +0600

    update(assets): images with logo

--- apps/edust/src/assets/images/index.ts
+++ apps/edust/src/assets/images/index.ts
@@ -1,11 +1,11 @@
-import logoLight from "./logo/logo-light.svg"
-import logoDark from "./logo/logo-dark.svg"
-import logoIconBoxLight from "./logo/logo-light-box.svg"
-import logoIconBoxDark from "./logo/logo-dark-box.svg"
-import iconsFacebook from "./icons/facebook.svg"
+import logoLight from "/images/logo/logo-light.svg"
+import logoDark from "/images/logo/logo-dark.svg"
+import logoIconBoxLight from "/images/logo/logo-light-box.svg"
+import logoIconBoxDark from "/images/logo/logo-dark-box.svg"
+import iconsFacebook from "/images/icons/facebook.svg"
 
 // For Organization
-import imgCreateOrganizations from "./organizations/create-organizations.png"
+import imgCreateOrganizations from "/images/organizations/create-organizations.png"
 
 const assets = {
   logoLight,
