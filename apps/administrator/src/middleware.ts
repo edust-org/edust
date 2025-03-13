8510087d9dba8895c2416c3c52abe78cd198283b
gmmamunh <gmmamun3004@gmail.com>
2025-03-13 11:06:25 +0600

    add(administrator): login form

--- apps/administrator/src/middleware.ts
+++ apps/administrator/src/middleware.ts
@@ -1,24 +1,24 @@
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
-import { Roles } from "./types"
+// import { Roles } from "./types"
 
 export default withAuth(
   function middleware(req) {
     if (!req.nextauth.token) {
-      return NextResponse.redirect(new URL("/auth/login", req.url))
+      return NextResponse.redirect(new URL("/login", req.url))
     }
 
     // Role-based access control
-    if (req.nextUrl.pathname.startsWith("/organizations")) {
-      const hasOrgOwner = req.nextauth.token.organizationRoles?.find(
-        (item) => item.role === Roles.OWNER,
-      )
+    // if (req.nextUrl.pathname.startsWith("/organizations")) {
+    //   const hasOrgOwner = req.nextauth.token.organizationRoles?.find(
+    //     (item) => item.role === Roles.OWNER,
+    //   )
 
-      if (!hasOrgOwner) {
-        return NextResponse.redirect(new URL("/", req.url)) // Redirect unauthorized users
-      }
-    }
+    //   if (!hasOrgOwner) {
+    //     return NextResponse.redirect(new URL("/", req.url)) // Redirect unauthorized users
+    //   }
+    // }
 
     return NextResponse.next()
   },
