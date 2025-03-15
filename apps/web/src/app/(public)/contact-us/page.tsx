28b7372202193df48c47b1dd42cb564cce923629
edustorg <edustorg@gmail.com>
2025-03-15 22:39:30 +0600

    feat(pages): empty about-us

--- apps/web/src/app/(public)/contact-us/page.tsx
+++ apps/web/src/app/(public)/contact-us/page.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@/components/ui"
 
-export default function ContactUS() {
+export default function ContactUs() {
   return (
     <>
       <Typography variant="h1">Contact Us</Typography>
--- apps/web/src/app/playground/page.tsx
+++ apps/web/src/app/playground/page.tsx
@@ -1,3 +1,21 @@
+"use client"
+
+import { defaultValues } from "@/configs"
+import axios from "@/lib/axios"
+
+import { useEffect } from "react"
+
 export default function Playground() {
+  useEffect(() => {
+    axios
+      .get(
+        `${defaultValues.backendURL}/api/v0/administrator/users
+`,
+      )
+      .then((res) => {
+        console.log(res)
+      })
+  }, [])
+
   return <>Playground</>
 }
