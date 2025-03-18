b18446ea9c3c7df6a66510b137736f9bbd2e44c8
khanRabiul <krabiulislam2@gmail.com>
2025-03-18 10:57:02 +0600

    feat(contact-us)page, contact form, contact info, newletter

--- apps/web/src/app/(public)/contact-us/page.tsx
+++ apps/web/src/app/(public)/contact-us/page.tsx
@@ -1,9 +1,36 @@
-import { Typography } from "@/components/ui"
+'use client'
+import { ContactNewsletter } from "./components/contact-newletter"
+import { ContactText } from "./components/contact-text"
+import { ContactFormSpree } from "./components/contact-form-spree"
+import { ContactHeading } from "./components/contact-heading"
+import { Footer, Navbar } from "@/components"
+
+
 
 export default function ContactUs() {
   return (
     <>
-      <Typography variant="h1">Contact Us</Typography>
+    <Navbar />
+      <section className="my-8 lg:my-16">
+        <div className="container">
+          <div className="text-center mb-10">
+            <ContactHeading />
+          </div>
+          <div className="flex flex-col md:flex-row md:items-center md:justify-evenly gap-10 mb-16">
+            <div>
+              <ContactFormSpree />
+            </div>
+            <div>
+              <ContactText />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <ContactNewsletter />
+      </section>
+      <Footer />
     </>
   )
 }
