83121656c6ee0789cb0db50097b541cc26ea3f88
gmmamunh <gmmamun3004@gmail.com>
2025-03-26 22:23:06 +0600

    add(administrator): feedback

---
+++ apps/administrator/src/app/feedback/loading.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { Skeleton } from "@/components/ui"
+
+export default function Loading() {
+  return (
+    <div className="space-y-4 p-4">
+      {/* Table header skeleton */}
+      <div className="flex justify-between">
+        <Skeleton className="h-6 w-32" />
+        <Skeleton className="h-6 w-20" />
+      </div>
+
+      {/* Table rows skeleton */}
+      <div className="space-y-2">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="flex items-center gap-4">
+            <Skeleton className="h-10 w-10 rounded-full" /> {/* Avatar */}
+            <Skeleton className="h-6 w-40" />
+            <Skeleton className="h-6 w-28" />
+            <Skeleton className="h-6 w-20" />
+            <Skeleton className="h-6 w-24" />
+            <Skeleton className="h-6 w-28" />
+            <Skeleton className="h-6 w-20" />
+            <Skeleton className="h-6 w-40" />
+            <Skeleton className="h-6 w-28" />
+            <Skeleton className="h-6 w-20" />
+            <Skeleton className="h-6 w-24" />
+            <Skeleton className="h-6 w-28" />
+            <Skeleton className="h-6 w-20" />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
