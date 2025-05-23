9eeb4c585b594f095074bcfdc7984d079429aa63
edustorg <edustorg@gmail.com>
2025-05-23 20:01:43 +0600

    update(pkg): version and institute editor

--- apps/web/src/app/(dashboard-root)/dashboard/institutes/create/editor/index.tsx
+++ apps/web/src/app/(dashboard-root)/dashboard/institutes/create/editor/index.tsx
@@ -1,14 +1,48 @@
 "use client"
 
 import { useThemeStore } from "@/store"
-import RcTiptapEditor, { BaseKit } from "reactjs-tiptap-editor"
+import RichTextEditor from "reactjs-tiptap-editor"
+import { BaseKit } from "reactjs-tiptap-editor"
+import { Attachment } from "reactjs-tiptap-editor/attachment"
 import { Blockquote } from "reactjs-tiptap-editor/blockquote"
 import { Bold } from "reactjs-tiptap-editor/bold"
 import { BulletList } from "reactjs-tiptap-editor/bulletlist"
 import { Clear } from "reactjs-tiptap-editor/clear"
 import { Code } from "reactjs-tiptap-editor/code"
 import { CodeBlock } from "reactjs-tiptap-editor/codeblock"
+import { Color } from "reactjs-tiptap-editor/color"
+import { Emoji } from "reactjs-tiptap-editor/emoji"
+import { ExportPdf } from "reactjs-tiptap-editor/exportpdf"
+import { ExportWord } from "reactjs-tiptap-editor/exportword"
+import { FontFamily } from "reactjs-tiptap-editor/fontfamily"
+import { FontSize } from "reactjs-tiptap-editor/fontsize"
+import { FormatPainter } from "reactjs-tiptap-editor/formatpainter"
+import { Heading } from "reactjs-tiptap-editor/heading"
+import { Highlight } from "reactjs-tiptap-editor/highlight"
+import { History } from "reactjs-tiptap-editor/history"
+import { HorizontalRule } from "reactjs-tiptap-editor/horizontalrule"
+import { Iframe } from "reactjs-tiptap-editor/iframe"
+import { ImportWord } from "reactjs-tiptap-editor/importword"
+import { Indent } from "reactjs-tiptap-editor/indent"
+import { Italic } from "reactjs-tiptap-editor/italic"
+import { Katex } from "reactjs-tiptap-editor/katex"
+import { LineHeight } from "reactjs-tiptap-editor/lineheight"
+import { Link } from "reactjs-tiptap-editor/link"
+import { Mention } from "reactjs-tiptap-editor/mention"
+import { Mermaid } from "reactjs-tiptap-editor/mermaid"
+import { MoreMark } from "reactjs-tiptap-editor/moremark"
+import { ColumnActionButton } from "reactjs-tiptap-editor/multicolumn"
+import { OrderedList } from "reactjs-tiptap-editor/orderedlist"
 import { SearchAndReplace } from "reactjs-tiptap-editor/searchandreplace"
+import { SlashCommand } from "reactjs-tiptap-editor/slashcommand"
+import { Strike } from "reactjs-tiptap-editor/strike"
+import { Table } from "reactjs-tiptap-editor/table"
+import { TableOfContents } from "reactjs-tiptap-editor/tableofcontent"
+import { TaskList } from "reactjs-tiptap-editor/tasklist"
+import { TextAlign } from "reactjs-tiptap-editor/textalign"
+import { TextDirection } from "reactjs-tiptap-editor/textdirection"
+import { TextUnderline } from "reactjs-tiptap-editor/textunderline"
+import { Twitter } from "reactjs-tiptap-editor/twitter"
 
 import React, { useCallback, useState } from "react"
 
@@ -45,7 +79,7 @@ const extensions = [
   FontSize,
   Bold,
   Italic,
-  Underline,
+  TextUnderline,
   Strike,
   MoreMark,
   Katex,
@@ -209,7 +243,7 @@ function Editor({ setContentHtml }: { setContentHtml: any }) {
             Documentation
           </button>
         </div> */}
-        <RcTiptapEditor
+        <RichTextEditor
           ref={refEditor}
           output="html"
           content={DEFAULT}
